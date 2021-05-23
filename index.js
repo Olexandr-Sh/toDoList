c99962dea979b7160affb2dfd3fc67b19c02d1af
Olexandr-Sh <shevtcov07@ukr.net>
2021-05-23 17:38:56 +0300

    created html elements and js file

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-class List {
-  createNewToDo() {
-    let newToDoBlock = document.createElement('div');
-    newToDoBlock.classList.add('newTask');
+// class List {
+//   attachTask() {
+//     let toDoElementsBlock = document.getElementsByClassName(toDoElements);
+//     let newToDoBlock = this.createNewToDo();
+//     toDoElementsBlock.appendChild(newToDoBlock);
+//   }
+//   createNewToDo() {
+//     let newToDoBlock = document.createElement('div');
+//     newToDoBlock.classList.add('newTask');
+    
+//     let listDateBlock = this.createListDateBlock();
+//     listDateBlock.appendChild(createNewToDo);
+    
+//     return newToDoBlock;
+//   }
 
-    return newToDoBlock;
-  }
-}
+//   createListDateBlock() {
+//     let listDateBlock = document.createElement('span');
+//     listDateBlock.classList.add('date');
+//     listDateBlock.textContent - '23.05.21';
 
-let list = new List();
+//     return listDateBlock;
+//   }
+// }
+
+// let list = new List();
+
+let clear = document.querySelector('.clear');
+
+let dateElement = document.getElementById('date');
+
+let list = document.getElementById('list');
+
+let input = document.getElementById('input');
